77a0876e584ef99e63f9a3acc4dc354ee908f393
EC2 Default User <ec2-user@ip-172-31-7-215.ec2.internal>
2021-05-10 15:33:58 +0000

    完成形提出１回目

--- main3.js
+++ main3.js
@@ -1,94 +1,60 @@
- //シングルクォーテーションとダブルクォーテーションはどっちでもいい
-   
-  //入力の値を格納
-  //let string = document.getElementById("filter");
-  /*
-  let tag1 = document.getElementById('tag1').value;
-  let tag2 = document.getElementById("tag2");
-  let tag3 = document.getElementById("tag3");
-  let tag4 = document.getElementById("tag4");
-  */
-
-  
-  function inputCheck() {
-    /*inputに入力された文字をinputValueに格納*/
+　　function inputCheck() {
+    /*input textに入力された文字をinputValueに格納。*/
+    /*document.getElementById("filter").value;の部分は他とは違って文字ではなく値(value)を入れるためvalueになってる。*/
     let inputValue = document.getElementById("filter").value;
     let tag1 = document.getElementById('tag1').textContent;
     let tag2 = document.getElementById("tag2").textContent;
     let tag3 = document.getElementById("tag3").textContent;
     let tag4 = document.getElementById("tag4").textContent;
       
-    //.idをつけると、block1だけ抽出されるけど、それだとだめ。
-    //これで値を格納するとdiv#block1で値が格納される。それで下の処理ができるようになる。
+    /*ここで一つずつ画像に仕掛けている。*/
     let block1 = document.getElementById("img1");
     let block2 = document.getElementById("img2");
     let block3 = document.getElementById("img3");
     let block4 = document.getElementById("img4");
     
       if ( inputValue =="") {
-      //inputValueが空欄なら入力なしを表示
-      document.getElementById('check').innerHTML = '入力前';
-      //入力がない時は全部表示する
-      block1.classList.remove("hide");
-      block2.classList.remove("hide");      
-      block3.classList.remove("hide");
-      block4.classList.remove("hide");
+      /*inputValueが空欄なら入力なしを表示*/
+      /*id名checkを入力してねと表示する。でも何もしないと「入力前」と表示されたままだが、一度入力してから文字を全部消すと「入力してね」の文字が表示される。*/
+      document.getElementById('check').innerHTML = '入力中してね';
+      /*次の４つは入力がない時は全部表示するようにする。*/
+      /*注意マークが出てるけど普通に動く。*/
+      img1.classList.remove("hide");
+      img2.classList.remove("hide");      
+      img3.classList.remove("hide");
+      img4.classList.remove("hide");
       
-      //alert("入力無しテスト");
     } else {
-      //inputValueに何かが入っていれば入力中とその入力内容を表示
+      /*inputValueに何かが入っていれば入力中とその入力内容を表示。犬と打った時に「入力中：犬」と枠の下の所に表示される。*/
       document.getElementById("check").innerHTML = '入力中：' + inputValue
       
-        //部分一致を判定する
-        
+        /*index0fで部分一致を判定する*/
+        /*-1は固定。−1で返した時にはimgを表示させる。*/
         if(tag1.indexOf(inputValue) != -1) {
-          //alert("部分一致あります");
-          //document.getElementById('check').innerHTML = 'test';
-          block1.classList.remove("hide");
-          //block1.classList.remove("test");
-        }else{
-          //部分一致がなかったときの処理
-          //alert("部分一致ないです");
-          block1.classList.add("hide");
-          //このtest classの追加と消去はできてるから、クラスの追加と消去はできてる。
-          //block1.classList.add("test");
-          
+          img1.classList.remove("hide");
+        }
+        /*それ以外はcssで設定したhideを追加する。これがないとimg1はずっと表示されてしまう。cssでnoneに設定してるから当てはまらないときは消えるようになる。*/
+        else{
+          img1.classList.add("hide");
         }
         
-        //この繰り返し処理を配列か何かでやりたい。tags=[]で格納するか？
-        //変数の宣言とかもそうしたい。
         if(tag2.indexOf(inputValue) != -1) {
-          block2.classList.remove("hide");
+          img2.classList.remove("hide");
         }else{
-          block2.classList.add("hide");
+          img2.classList.add("hide");
         }        
         
         if(tag3.indexOf(inputValue) != -1) {
-          block3.classList.remove("hide");
+          img3.classList.remove("hide");
         }else{
-          block3.classList.add("hide");
+          img3.classList.add("hide");
         }        
         
         if(tag4.indexOf(inputValue) != -1) {
-          block4.classList.remove("hide");
+          img4.classList.remove("hide");
         }else{
-          block4.classList.add("hide");
+          img4.classList.add("hide");
         }
-        
-        //各タグに関して部分一致を判定する
-        /*
-        for(let i=1; i<5; i++){
-          
-          if(tag1.indexOf(inputValue) != -1) {
-            block1.classList.remove("hide");
-          }else{
-            block1.classList.add("hide");
-          }
-        }
-        */
-      
-      
-      
     }
     
   }
